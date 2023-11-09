81f37c84e6b0b56ae3590efbd1e681961a339782
NightDevilPT <pawankumartadagsingh@gmail.com>
2023-11-09 07:11:29 +0530

    vercel-config

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook: env.VERCEL_HOOK,
-      apiToken: env.VERCEL_TOKEN,
+      deployHook: process.env.VERCEL_HOOK,
+      apiToken: process.env.VERCEL_TOKEN,
       appFilter: "strapi-backend",
       teamFilter: "your-team-id-on-vercel",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
